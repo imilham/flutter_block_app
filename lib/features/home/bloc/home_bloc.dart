53a029641682b9fe85207bc1881cfc644292b8c9
imilham <im.ilhamk@gmail.com>
2024-04-21 17:44:36 +0530

    first commit

---
+++ lib/features/home/bloc/home_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<HomeProductWishlistButtonClickedEvent>(
+        homeProductWishlistButtonClickedEvent);
+    on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
+    on<HomeWishlistButtonNavigateEvent>(homeWishlistButtonNavigateEvent);
+    on<HomeCartButtonNavigateEvent>(homeCartButtonNavigateEvent);
+  }
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(
+      HomeProductWishlistButtonClickedEvent event, Emitter<HomeState> emit) {
+    print('Wishlist Product clicked');
+  }
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+      HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {
+    print('Cart Product clicked');
+  }
+
+  FutureOr<void> homeWishlistButtonNavigateEvent(
+      HomeWishlistButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print('Wishlist Navigate clicked');
+    emit(HomeNavigateToWishlistPageActionState());
+  }
+
+  FutureOr<void> homeCartButtonNavigateEvent(
+      HomeCartButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print('Cart Navigate clicked');
+    emit(HomeNavigateToCartPageActionState());
+  }
+}
---
+++ lib/features/home/ui/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_block_app/features/cart/ui/cart.dart';
+import 'package:flutter_block_app/features/home/bloc/home_bloc.dart';
+import 'package:flutter_block_app/features/wishlist/ui/wishlist.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final HomeBloc homeBloc = HomeBloc();
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listenWhen: (previous, current) => current is HomeActionState,
+      buildWhen: (previous, current) => current is! HomeActionState,
+      listener: (context, state) {
+        // TODO: implement listener
+        if (state is HomeNavigateToCartPageActionState) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Cart()));
+        } else if (state is HomeNavigateToWishlistPageActionState) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Wishlist()));
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.teal, // Set the background color to blue
+            title: Text('Grocery App'),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    homeBloc.add(HomeWishlistButtonNavigateEvent());
+                  },
+                  icon: Icon(Icons.favorite)),
+              IconButton(
+                  onPressed: () {
+                    homeBloc.add(HomeCartButtonNavigateEvent());
+                  },
+                  icon: Icon(Icons.shopping_bag_outlined))
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
